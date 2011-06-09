6a046d414fcb78916a4efa1ecdf754fcf68da606
Franco Bulgarelli <franco@zaubersoftware.com>
2011-06-09 16:42:27 -0300

    Adding demo

---
+++ example-report-issue/README.txt
@@ -0,0 +1,15 @@
+Jira Search and Upload Demo
+==============================
+
+INTRODUCTION
+  This is minimalistic a demo creating issues in a Jira Tracker. 
+
+HOW TO DEMO:
+  1. Set the following system properties:
+    a. jiraUsername This is the username of your Jira Account
+    b. jiraPassword This is the password of your Jira account
+  	c. jiraAddress This is the url of your Jira server
+  2. Run the "MainFlow". This will create an issue given its description and summary. You can run it by either running the JiraFunctionalTestDriver or hitting 
+  http://localhost:9090/jira-demo-report-issue?summary=<SUMMARY>&description=<DESCRIPTION>
+
+    
---
+++ example-report-issue/src/test/java/org/mulesoft/demo/jira/JiraFunctionalTestDriver.java
@@ -0,0 +1,42 @@
+/**
+ * Mule Jira Cloud Connector
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mulesoft.demo.jira;
+
+import org.mule.api.MuleEvent;
+import org.mule.api.transport.PropertyScope;
+import org.mule.construct.SimpleFlowConstruct;
+import org.mule.tck.FunctionalTestCase;
+
+public class JiraFunctionalTestDriver extends FunctionalTestCase
+{
+    @Override
+    protected String getConfigResources()
+    {
+        return "mule-config.xml";
+    }
+
+    public void testUpdateFlow() throws Exception
+    {
+        final MuleEvent event = getTestEvent("");
+        event.getMessage().setProperty("summary", "Titles are wrong cased", PropertyScope.INBOUND);
+        event.getMessage().setProperty("description",
+            "Titles should start with an uppercase letter, but ...", PropertyScope.INBOUND);
+        final SimpleFlowConstruct flow = lookupFlowConstruct("MainFlow");
+        final MuleEvent responseEvent = flow.process(event);
+        System.out.println(responseEvent);
+    }
+
+    private SimpleFlowConstruct lookupFlowConstruct(final String name)
+    {
+        return (SimpleFlowConstruct) muleContext.getRegistry().lookupFlowConstruct(name);
+    }
+
+}
