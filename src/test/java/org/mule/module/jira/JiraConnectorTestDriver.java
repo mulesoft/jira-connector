232d293f3d26fa2fae23e639deb0f86707fed6bb
Pablo Diez <pablo@zauberlabs.com>
2012-06-19 15:17:48 -0300

    Adding ignored test driver for update-issues-by-jql

--- src/test/java/org/mule/module/jira/JiraConnectorTestDriver.java
+++ src/test/java/org/mule/module/jira/JiraConnectorTestDriver.java
@@ -8,32 +8,57 @@
  * LICENSE.txt file.
  */
 
- package org.mule.module.jira;
+package org.mule.module.jira;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mule.api.ConnectionException;
 import org.mule.module.jira.api.AxisJiraClient;
 import org.mule.module.jira.api.DefaultAxisPortProvider;
 
 /**
  * Test driver for {@link JiraConnector}
- * */
+ */
 public class JiraConnectorTestDriver
 {
-    private JiraConnector connector;
     private String endpoint = System.getenv("jira.endpoint");
+    private String username = System.getenv("jira.username");
+    private String password = System.getenv("jira.password");
     
+    private JiraConnector connector;
+
     @Before
-    public void init()
+    public void init() throws ConnectionException
     {
         this.connector = new JiraConnector();
         connector.setClient(new AxisJiraClient(new DefaultAxisPortProvider(endpoint)));
+        connector.connect(username, password, endpoint);
     }
-    
+
     @Test(expected = JiraConnectorException.class)
     public void createIssue()
     {
         connector.createIssue(null, null, null, null, null, null, null, null, null, null, null, null, null);
     }
-}
 
+    @Test
+    @Ignore
+    public void updateIssuesByJql()
+    {
+        Map<String, List<String>> fields = new HashMap<String, List<String>>();
+        fields.put("priority", new ArrayList<String>()
+        {
+            {
+                add("1");
+            }
+        });
+        connector.updateIssuesByJql("status = Open", fields, null);
+    }
+
+}
